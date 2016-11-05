1d3fee5e1bd6b3920f79a0a8b35ef639c6e092be
TeamVader <alex.stark@hotmail.com>
2016-11-05 12:20:28 +0100

    Init Again

--- WifiManager/WifiManager.cs
+++ WifiManager/WifiManager.cs
@@ -86,8 +86,7 @@ namespace WifiManager
                 byte[] ssidBytes = System.Text.Encoding.Default.GetBytes(ssid);
                 string ssidHex = BitConverter.ToString(ssidBytes);
                 ssidHex = ssidHex.Replace("-", "");
-                  //  ssidHex = ssidHex.Replace("_", "");
-                   // ssidHex = "53756E726973655F322E3447487A5F363546303430";
+             
                 string key = "";
                 string profileXml = string.Format("<?xml version=\"1.0\"?><WLANProfile xmlns=\"http://www.microsoft.com/networking/WLAN/profile/v1\"><name>{0}</name><SSIDConfig><SSID><hex>{1}</hex><name>{0}</name></SSID></SSIDConfig><connectionType>ESS</connectionType><connectionMode>manual</connectionMode><MSM><security><authEncryption><authentication>WPA2PSK</authentication><encryption>AES</encryption><useOneX>false</useOneX></authEncryption><sharedKey><keyType>passPhrase</keyType><protected>false</protected><keyMaterial>{2}</keyMaterial></sharedKey></security></MSM></WLANProfile>",ssid,ssidHex,key);
                 
